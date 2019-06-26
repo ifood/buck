4ab8c7b2e9684bc4071f7549d91c3151af223f5f
Bruno Rocha <brunorochaesilva@gmail.com>
2019-06-26 17:44:58 -0300

    Don't kill simulators

--- src/com/facebook/buck/cli/InstallCommand.java
+++ src/com/facebook/buck/cli/InstallCommand.java
@@ -69,7 +69,6 @@ import com.facebook.buck.util.ExitCode;
 import com.facebook.buck.util.ListeningProcessExecutor;
 import com.facebook.buck.util.MoreExceptions;
 import com.facebook.buck.util.ProcessExecutor;
-import com.facebook.buck.util.UnixUserIdFetcher;
 import com.facebook.infer.annotation.Assertions;
 import com.facebook.infer.annotation.SuppressFieldNotInitialized;
 import com.google.common.annotations.VisibleForTesting;
@@ -803,31 +802,10 @@ public class InstallCommand extends BuildCommand {
       return FAILURE;
     }
 
-    UnixUserIdFetcher userIdFetcher = new UnixUserIdFetcher();
     AppleCoreSimulatorServiceController appleCoreSimulatorServiceController =
         new AppleCoreSimulatorServiceController(processExecutor);
 
-    Optional<Path> coreSimulatorServicePath =
-        appleCoreSimulatorServiceController.getCoreSimulatorServicePath(userIdFetcher);
-
     boolean shouldWaitForSimulatorsToShutdown = false;
-
-    if (!coreSimulatorServicePath.isPresent()
-        || !coreSimulatorServicePath
-            .get()
-            .toRealPath()
-            .startsWith(xcodeDeveloperPath.get().toRealPath())) {
-      LOG.warn(
-          "Core simulator service path %s does not match developer directory %s, "
-              + "killing all simulators.",
-          coreSimulatorServicePath, xcodeDeveloperPath.get());
-      if (!appleCoreSimulatorServiceController.killSimulatorProcesses()) {
-        params.getConsole().printBuildFailure("Could not kill running simulator processes.");
-        return FAILURE;
-      }
-
-      shouldWaitForSimulatorsToShutdown = true;
-    }
 
     Path simctlPath = xcodeDeveloperPath.get().resolve("usr/bin/simctl");
     Optional<AppleSimulator> appleSimulator =
