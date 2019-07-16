3d8033bccee20308cba12e31fc4da01186be5072
Bruno Rocha <brunorochaesilva@gmail.com>
2019-07-16 10:58:00 -0300

    Remove stupid boundaries for paths

--- src/com/facebook/buck/parser/ThrowingPackageBoundaryChecker.java
+++ src/com/facebook/buck/parser/ThrowingPackageBoundaryChecker.java
@@ -62,18 +62,6 @@ public class ThrowingPackageBoundaryChecker implements PackageBoundaryChecker {
     boolean isBasePathEmpty = basePath.isEmpty();
 
     for (ForwardRelativePath path : paths) {
-      if (!isBasePathEmpty && !path.startsWith(basePath)) {
-        String formatString = "'%s' in '%s' refers to a parent directory.";
-        warnOrError(
-            enforcing,
-            formatString,
-            basePath
-                .toPath(targetCell.getFilesystem().getFileSystem())
-                .relativize(path.toPath(targetCell.getFilesystem().getFileSystem())),
-            target);
-        continue;
-      }
-
       Optional<RelPath> ancestor =
           buildFileTree.getBasePathOfAncestorTarget(
               path.toRelPath(targetCell.getFilesystem().getFileSystem()));
@@ -88,27 +76,6 @@ public class ThrowingPackageBoundaryChecker implements PackageBoundaryChecker {
                 "Target '%s' refers to file '%s', which doesn't belong to any package. "
                     + "More info at:\nhttps://buck.build/about/overview.html\n",
                 target, path));
-      }
-
-      if (!ancestor.get().equals(basePath.toRelPath(targetCell.getFilesystem().getFileSystem()))) {
-        String buildFileName = targetCell.getBuckConfigView(ParserConfig.class).getBuildFileName();
-        Path buckFile = ancestor.get().resolve(buildFileName);
-        // TODO(cjhopman): If we want to manually split error message lines ourselves, we should
-        // have a utility to do it correctly after formatting instead of doing it manually.
-        String formatString =
-            "The target '%1$s' tried to reference '%2$s'.\n"
-                + "This is not allowed because '%2$s' can only be referenced from '%3$s' \n"
-                + "which is its closest parent '%4$s' file.\n"
-                + "\n"
-                + "You should find or create a rule in '%3$s' that references\n"
-                + "'%2$s' and use that in '%1$s'\n"
-                + "instead of directly referencing '%2$s'.\n"
-                + "More info at:\nhttps://buck.build/concept/build_rule.html\n"
-                + "\n"
-                + "This issue might also be caused by a bug in buckd's caching.\n"
-                + "Please check whether using `buck kill` resolves it.";
-
-        warnOrError(enforcing, formatString, target, path, buckFile, buildFileName);
       }
     }
   }
